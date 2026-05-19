78659a199291fea1898b7b839c715cf45df5e85c
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-19 11:52:37 +0600

    Time: 434 ms (79.62%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,7 +1,12 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT 
     s.user_id,
-    ROUND(IFNULL(SUM(c.action = 'confirmed')/COUNT(c.user_id), 0) ,2) AS confirmation_rate
+    ROUND(
+        ISNULL(
+            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)*1.0/
+            NULLIF(COUNT(c.user_id), 0)
+        , 0) 
+    , 2) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c
 ON s.user_id = c.user_id
