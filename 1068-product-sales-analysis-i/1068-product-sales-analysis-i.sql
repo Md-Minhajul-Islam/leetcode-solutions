fc8ba7df118d454d8cead82269b7f6442f88fbde
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-19 11:37:23 +0600

    Time: 2761 ms (67.63%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT s.year as year, s.price as price, p.product_name as product_name
 FROM Sales s
 JOIN Product p
