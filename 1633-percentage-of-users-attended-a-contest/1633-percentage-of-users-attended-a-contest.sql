9a766bcca7ec5a46dc78cebf53b90c3f4781b1f8
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-19 11:56:08 +0600

    Time: 3140 ms (9.72%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 SELECT 
 r.contest_id,
 ROUND(COUNT(DISTINCT r.user_id)*100.0/(SELECT COUNT(*) FROM Users) , 2) AS percentage
