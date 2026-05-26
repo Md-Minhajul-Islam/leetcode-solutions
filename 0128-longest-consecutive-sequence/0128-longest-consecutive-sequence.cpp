c519460df45e5c35edf2c093908550840d4c960b
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-26 11:55:17 +0600

    Time: 156 ms (8.44%), Space: 117.6 MB (5.16%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        unordered_map<int, int> hashMap, vis;
+        for(auto &n: nums) hashMap[n] = 1;
+
+        int mx = 0;
+        for(auto &n: nums)
+        {
+            if(vis.count(n)) continue;
+            if(hashMap.count(n))
+            {
+                int cnt = 0;
+                for(int i = n; hashMap.count(i); i++)
+                {
+                    vis[i] = 1;
+                    cnt++;
+                }
+                for(int i = n-1; hashMap.count(i); i--)
+                {
+                    vis[i] = 1;
+                    cnt++;
+                }
+                mx = max(mx, cnt);
+            }
+        }
+        return mx;
+    }
+};
