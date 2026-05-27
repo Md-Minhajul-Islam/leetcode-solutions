153f7d958b92e26b1b17459002b5c36ea938d3f8
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-27 15:46:50 +0600

    Time: 542 ms (31.3%), Space: 97.4 MB (13.91%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+
+        unordered_map<string, bool> hashMap;
+        for(auto& idea: ideas) hashMap[idea] = true;
+
+        vector<vector<int>> v(30, vector<int>(30));
+
+        for(auto& idea: ideas)
+        {
+            int r = idea[0]-'a';
+            for(int c = 0; c < 26; c++)
+            {
+                if(r == c) continue;
+                idea[0] = 'a'+c;
+                if(!hashMap.count(idea)) v[r][c]++;
+            }
+        }
+
+        long long ans = 0;
+        for(int i = 0; i < 26; i++)
+        {
+            for(int j = 0; j < 26; j++)
+            {
+                ans += v[i][j]*v[j][i];
+            }
+            cout << "\n";
+        }
+        return ans;
+    }
+};
