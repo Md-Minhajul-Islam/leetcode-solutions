4f180c0a4e6b36ab47c18ec61acd4cdbedad0af1
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-28 10:37:43 +0600

    Time: 8 ms (99.18%), Space: 134.5 MB (96.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,10 +5,10 @@ public:
         vector<int> ans(nums.size()-k+1);
         for(int i = 0; i < nums.size(); i++)
         {
+            while(!dq.empty() && dq.front() <= i-k) dq.pop_front();
             while(!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
             dq.push_back(i);
             if(i < k-1) continue;
-            while(dq.front() <= i-k) dq.pop_front();
             ans[i-k+1] = nums[dq.front()];
         }
         return ans;
