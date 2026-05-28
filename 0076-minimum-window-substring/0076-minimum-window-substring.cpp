c6215cbacb87e2af829cce5a11834962bc0bd19d
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-28 09:35:11 +0600

    Time: 0 ms (100%), Space: 11.3 MB (81.34%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> hashT, hashS;
-        for(auto& c: t) hashT[c]++;
+        vector<int> hashT(60), hashS(60);
+        int required = 0, formed = 0;
+        for(auto& c: t)
+        {
+            if(!hashT[c-'A']) required++;
+            hashT[c-'A']++;
+        }
         int l = 0, r = 0, mn = 1e6, i = 1e6, j = -1;
-        int required = hashT.size(), formed = 0;
         while(r < s.size())
         {
-            hashS[s[r]]++;
-            if(hashT.count(s[r]) && hashT[s[r]] == hashS[s[r]]) formed++;
+            hashS[s[r]-'A']++;
+            if(hashT[s[r]-'A'] && hashT[s[r]-'A'] == hashS[s[r]-'A']) formed++;
             while(required == formed)
             {
                 if(r-l+1 < mn)
@@ -16,8 +20,8 @@ public:
                     mn = r-l+1;
                     i = l; j = r;
                 }
-                hashS[s[l]]--;
-                if(hashT.count(s[l]) && hashS[s[l]] < hashT[s[l]]) formed--;
+                hashS[s[l]-'A']--;
+                if(hashT[s[l]-'A'] && hashS[s[l]-'A'] < hashT[s[l]-'A']) formed--;
                 l++;
             }
             r++;
