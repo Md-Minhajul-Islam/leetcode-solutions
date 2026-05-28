cb7fcc0c3ce82fffacdb5a8675620f3aad40c2e0
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-05-28 09:29:14 +0600

    Time: 6 ms (63.64%), Space: 11.4 MB (62.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,11 +3,13 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char, int> hashT, hashS;
         for(auto& c: t) hashT[c]++;
-        int l = 0, r = 0, mn = s.size()+1, i = s.size(), j = -1;
+        int l = 0, r = 0, mn = 1e6, i = 1e6, j = -1;
+        int required = hashT.size(), formed = 0;
         while(r < s.size())
         {
             hashS[s[r]]++;
-            while(countMatching(hashT, hashS))
+            if(hashT.count(s[r]) && hashT[s[r]] == hashS[s[r]]) formed++;
+            while(required == formed)
             {
                 if(r-l+1 < mn)
                 {
@@ -15,22 +17,12 @@ public:
                     i = l; j = r;
                 }
                 hashS[s[l]]--;
+                if(hashT.count(s[l]) && hashS[s[l]] < hashT[s[l]]) formed--;
                 l++;
             }
             r++;
         }
-        string ans = "";
-        for(int idx = i; idx <= j; idx++) ans += s[idx];
-        return ans;
-    }
-    bool countMatching(unordered_map<char, int>& hashT,
-        unordered_map<char, int>& hashS)
-    {
-        for(auto& [ch, cnt]: hashT)
-        {
-            if(hashS.count(ch) && hashS[ch] >= cnt) continue;
-            return false;
-        }
-        return true;
+        if(i > j) return "";
+        return s.substr(i, j-i+1);
     }
 };
