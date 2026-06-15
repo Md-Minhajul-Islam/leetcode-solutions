39ee4ba6b8a61e2a2fd44805b63b86df7fe5c394
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-06-15 14:27:59 +0600

    Time: 4 ms (51.73%), Space: 15 MB (18.68%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(hashMap.count(target-nums[i]))
+            {
+                return {hashMap[target-nums[i]], i};
+            }
+            hashMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
