9179c8a4cd4003a4f050ca3b5739a072e22f3959
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-06-17 12:11:47 +0600

    Time: 589 ms (12.76%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,8 +1,12 @@
 /* Write your T-SQL query statement below */
-WITH ranked AS (
-    SELECT *,
-    DENSE_RANK() OVER (PARTITION BY email ORDER BY id) AS rn
+
+WITH rankedTable AS (
+    SELECT
+    *,
+    DENSE_RANK() OVER(PARTITION BY email ORDER BY id) as rank
     FROM Person
 )
-DELETE FROM ranked
-WHERE rn > 1;
+
+DELETE
+FROM rankedTable 
+WHERE rank > 1
