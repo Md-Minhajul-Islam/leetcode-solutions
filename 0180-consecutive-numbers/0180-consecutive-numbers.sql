6bedb80e17162b5d5d97f5b62c24bb03c9c69bef
Md Minhajul Islam <129651904+Md-Minhajul-Islam@users.noreply.github.com>
2026-06-17 11:28:04 +0600

    Time: 555 ms (52.46%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,7 +1,12 @@
 /* Write your T-SQL query statement below */
 SELECT
 DISTINCT(l1.num) AS ConsecutiveNums
-FROM Logs l1
-JOIN Logs l2 ON l1.num = l2.num
-JOIN Logs l3 ON l1.num = l3.num
-WHERE l1.id = l2.id-1 AND l2.id = l3.id-1;
+FROM 
+    Logs l1, 
+    Logs l2, 
+    Logs l3
+WHERE 
+    l1.id+1 = l2.id AND 
+    l2.id+1 = l3.id AND
+    l1.num = l2.num AND
+    l2.num = l3.num;
